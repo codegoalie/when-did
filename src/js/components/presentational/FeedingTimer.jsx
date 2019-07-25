74290e078bf23acfc649dab2fa1bca434185d013
Chris Marshall <chris@codegoalie.com>
2019-07-24 20:25:40 -0400

    Working timer

---
+++ src/js/components/presentational/FeedingTimer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const FeedingTimer = ({ timerTime }) => {
+  let centiseconds = ("0" + (Math.floor(timerTime / 10) % 100)).slice(-2);
+  let seconds = ("0" + (Math.floor(timerTime / 1000) % 60)).slice(-2);
+  let minutes = ("0" + (Math.floor(timerTime / 60000) % 60)).slice(-2);
+  let hours = ("0" + Math.floor(timerTime / 3600000)).slice(-2);
+  return (
+    <div className="dummy">
+      {hours}:{minutes}:{seconds}.{centiseconds}
+    </div>
+  );
+};
+
+FeedingTimer.propTypes = {
+  timerTime: PropTypes.number.isRequired
+};
+
+export default FeedingTimer;
